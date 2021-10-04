385821cb44b888280fd5a9f0d4d052bc987b972e
Tyler <tyler.felsted@yahoo.com>
2021-10-04 00:04:10 -0400

    configured project

---
+++ db.js
@@ -0,0 +1,15 @@
+"use strict";
+/** Database setup. */
+const { MongoClient } = require("mongodb");
+const { getDatabaseUri } = require("./config");
+
+let db = new MongoClient(getDatabaseUri());
+
+
+try {
+  db.connect();
+} catch(err) {
+  console.error(err);
+}
+
+module.exports = db;
